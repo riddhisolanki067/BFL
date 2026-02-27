c5a27198b2aaf219822b639b7852c9676c3212c2
Riddhi Solanki <riddhisolanki067@gmail.com>
2026-02-28 00:00:20 +0530

    SQL error

--- bfl_custom/bfl_custom/report/raw_material_daily_stock/raw_material_daily_stock.py
+++ bfl_custom/bfl_custom/report/raw_material_daily_stock/raw_material_daily_stock.py
@@ -3,28 +3,28 @@ import frappe
 def execute(filters=None):
 
     filters = filters or {}
-    from_date = filters.get("from_date")
-    to_date = filters.get("to_date")
+	from_date = filters.get("from_date")
+	to_date = filters.get("to_date")
 
-    columns = [
-        {"label": "Item", "fieldname": "item", "fieldtype": "Data", "width": 150},
-        {"label": "Batch", "fieldname": "batch_count", "fieldtype": "Data", "width": 100},
-        {"label": "Batch Size", "fieldname": "batch_size", "fieldtype": "Data", "width": 100},
-        {"label": "Opening", "fieldname": "opening", "fieldtype": "Float", "width": 100},
-        {"label": "Loan", "fieldname": "loan", "fieldtype": "Float", "width": 100},
-        {"label": "Purchase", "fieldname": "purchase", "fieldtype": "Float", "width": 100},
-        {"label": "Closing", "fieldname": "closing", "fieldtype": "Float", "width": 100},
-        {"label": "Consumption", "fieldname": "consumption", "fieldtype": "Float", "width": 120},
-    ]
+	columns = [
+		{"label": "Item", "fieldname": "item", "fieldtype": "Data", "width": 150},
+		{"label": "Batch", "fieldname": "batch_count", "fieldtype": "Data", "width": 100},
+		{"label": "Batch Size", "fieldname": "batch_size", "fieldtype": "Data", "width": 100},
+		{"label": "Opening", "fieldname": "opening", "fieldtype": "Float", "width": 100},
+		{"label": "Loan", "fieldname": "loan", "fieldtype": "Float", "width": 100},
+		{"label": "Purchase", "fieldname": "purchase", "fieldtype": "Float", "width": 100},
+		{"label": "Closing", "fieldname": "closing", "fieldtype": "Float", "width": 100},
+		{"label": "Consumption", "fieldname": "consumption", "fieldtype": "Float", "width": 120},
+	]
 
-    data = frappe.db.sql("""
-        SELECT
-            child.item AS item,
-            parent.batch AS batch_count,
-            parent.batch_size AS batch_size,
-            SUM(child.opening) AS opening,
-            SUM(child.loan) AS loan,
-            SUM(child.purchase) AS purchase,
+	data = frappe.db.sql("""
+		SELECT
+			child.item AS item,
+			parent.batch AS batch_count,
+			parent.batch_size AS batch_size,
+			SUM(child.opening) AS opening,
+			SUM(child.loan) AS loan,
+			SUM(child.purchase) AS purchase,
 			SUM(child.closing) AS closing,
 			SUM(child.consumption) AS consumption
 		FROM `tabRM Consumpation Item` child
@@ -35,23 +35,24 @@ def execute(filters=None):
 		ORDER BY child.item
 	""", (from_date, to_date), as_dict=1)
 
-    # Optional: Add Total Row
-    total = {
-        "item": "TOTAL",
-        "opening": 0,
-        "loan": 0,
-        "purchase": 0,
-        "closing": 0,
-        "consumption": 0,
-    }
+	total = {
+		"item": "TOTAL",
+		"opening": 0,
+		"loan": 0,
+		"purchase": 0,
+		"closing": 0,
+		"consumption": 0,
+		"batch_count": "",
+		"batch_size": "",
+	}
 
-    for row in data:
-        total["opening"] += row.opening or 0
-        total["loan"] += row.loan or 0
-        total["purchase"] += row.purchase or 0
-        total["closing"] += row.closing or 0
-        total["consumption"] += row.consumption or 0
+	for row in data:
+		total["opening"] += row.opening or 0
+		total["loan"] += row.loan or 0
+		total["purchase"] += row.purchase or 0
+		total["closing"] += row.closing or 0
+		total["consumption"] += row.consumption or 0
 
-    data.append(total)
+	data.append(total)
 
-    return columns, data
+	return columns, data
