92a83cd8f619815d2bf9fe716bd9ff5108568c4c
Riddhi Solanki <riddhisolanki067@gmail.com>
2026-03-11 14:14:17 +0530

    date validation

--- bfl_custom/bfl_custom/doctype/packing_production/packing_production.js
+++ bfl_custom/bfl_custom/doctype/packing_production/packing_production.js
@@ -4,5 +4,25 @@
 frappe.ui.form.on("Packing Production", {
 	 after_save(frm) {
         frappe.new_doc("Packing Production");
+    },
+    date(frm) {
+        if (!frm.doc.date) return;
+
+        return frappe.call({
+            method: "frappe.client.get_list",
+            args: {
+                doctype: "Packing Production",
+                filters: {
+                    date: frm.doc.date,
+                    name: ["!=", frm.doc.name]  // exclude current doc while editing
+                },
+                fields: ["name"],
+                limit_page_length: 1
+            }
+        }).then(r => {
+            if (r.message && r.message.length > 0) {
+                frappe.throw("Packing Production already exists for this date.");
+            }
+        });
     }
 });
