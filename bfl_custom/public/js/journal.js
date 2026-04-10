eed4efc030c9380d866f26c17c916958f26a0ca0
Riddhi Solanki <riddhisolanki067@gmail.com>
2026-04-10 13:46:22 +0530

    custom_employee

--- bfl_custom/public/js/journal.js
+++ bfl_custom/public/js/journal.js
@@ -66,7 +66,7 @@ function show_dialog(frm, data) {
                     { fieldname: "journal_entry", in_list_view: 1, label: "Journal Entry", fieldtype: "Link", options: "Journal Entry" },
                     { fieldname: "amount", in_list_view: 1, label: "Amount", fieldtype: "Currency" },
                     { fieldname: "custom_month", in_list_view: 1, label: "Month", fieldtype: "Data" },
-                    { fieldname: "custom_employee", in_list_view: 1, label: "Party", fieldtype: "Data" },
+                    { fieldname: "custom_employee", in_list_view: 1, label: "Employee", fieldtype: "Data" },
                     { fieldname: "custom_type", in_list_view: 1, label: "Type", fieldtype: "Data" }
   
                 ]
