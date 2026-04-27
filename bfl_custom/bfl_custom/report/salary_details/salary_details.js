8eb702ad8ad01ddd53b0abc8364b35cc98167d40
Riddhi Solanki <riddhisolanki067@gmail.com>
2026-04-27 11:19:52 +0530

    salary details

--- bfl_custom/bfl_custom/report/salary_details/salary_details.js
+++ bfl_custom/bfl_custom/report/salary_details/salary_details.js
@@ -1,30 +1,145 @@
+const _salary_row_data = {};
+
+const COL = {
+    employee: 0,
+    employee_name: 1,
+    type: 2,
+    payment_type: 3,
+    days_present: 4,
+    per_day_salary: 5,
+    total_salary: 6,
+    other_addings: 7,
+    other_deduction: 8,
+    amount: 9,
+    total_payable: 10,
+    advance_date: 11,
+    advance_amount: 12,
+    loan_date: 13,
+    loan_amount: 14,
+    deduction_amount: 15,
+    balance: 16,
+};
+
+const EDITABLE_FIELDS = ["days_present", "other_addings", "other_deduction"];
+const CALCULATED_FIELDS = ["total_salary", "amount", "total_payable", "balance"];
+
 frappe.query_reports["Salary Details"] = {
     filters: [
-			{
-			fieldname: "month",
-			label: "Month",
-			fieldtype: "Select",
-			options: [
-				"January",
-				"February",
-				"March",
-				"April",
-				"May",
-				"June",
-				"July",
-				"August",
-				"September",
-				"October",
-				"November",
-				"December"
-			],
-			reqd: 1
-		},
         {
-            fieldname: "employee",
-            label: "Employee",
+            fieldname: "month",
+            label: __("Month"),
+            fieldtype: "Select",
+            options: "\nJanuary\nFebruary\nMarch\nApril\nMay\nJune\nJuly\nAugust\nSeptember\nOctober\nNovember\nDecember",
+            reqd: 1
+        },
+        {
+            fieldname: "type",
+            label: __("Type"),
+            fieldtype: "Select",
+            options: "\nWeekly Salary\nAdvance Salary\nAdvance\nLoan",
+        },
+        {
+            fieldname: "payment_type",
+            label: __("Payment Type"),
+            fieldtype: "Select",
+            options: "\nBank\nCash",
+        },
+        {
+            fieldname: "employment_type",
+            label: __("Employment Type"),
             fieldtype: "Link",
-            options: "Employee"
+            options: "Employment Type",
+        },
+    ],
+
+    formatter(value, row, column, data, default_formatter) {
+        if (!data || !data.employee) {
+            return default_formatter(value, row, column, data);
         }
-    ]
+
+        const eid = data.employee;
+
+        if (!_salary_row_data[eid]) {
+            _salary_row_data[eid] = {
+                days_present: flt(data.days_present),
+                per_day_salary: flt(data.per_day_salary),
+                other_addings: flt(data.other_addings),
+                other_deduction: flt(data.other_deduction),
+                advance_amount: flt(data.advance_amount),
+                loan_amount: flt(data.loan_amount),
+                deduction_amount: flt(data.deduction_amount),
+            };
+        }
+
+        const rv = _salary_row_data[eid];
+        const computed = calc_all(rv);
+
+        if (EDITABLE_FIELDS.includes(column.fieldname)) {
+            return `<input
+                type="number"
+                class="salary-editable-input"
+                data-employee="${eid}"
+                data-field="${column.fieldname}"
+                value="${rv[column.fieldname] || 0}"
+                min="0"
+                step="any"
+                style="width:100%; text-align:right;"
+            >`;
+        }
+
+        if (column.fieldname === "total_salary") {
+            return fmt_currency(computed.total_salary);
+        }
+        if (column.fieldname === "amount") {
+            return fmt_currency(computed.amount);
+        }
+        if (column.fieldname === "total_payable") {
+            return fmt_currency(computed.total_payable);
+        }
+        if (column.fieldname === "balance") {
+            const color = computed.balance < 0
+                ? "color:#e53935;font-weight:700;"
+                : "color:#2e7d32;font-weight:600;";
+            return `<span style="${color}">${fmt_currency(computed.balance)}</span>`;
+        }
+
+        return default_formatter(value, row, column, data);
+    },
+
+    after_datatable_render(datatable) {
+        setup_input_handlers();
+    }
 };
+
+function flt(v) {
+    return parseFloat(v) || 0;
+}
+
+function fmt_currency(v) {
+    return frappe.format(v, { fieldtype: "Currency" });
+}
+
+function calc_all(rv) {
+    const total_salary = flt(rv.days_present) * flt(rv.per_day_salary);
+    const amount = flt(rv.other_addings) - flt(rv.other_deduction);
+    const total_payable = total_salary + amount;
+    const balance = total_payable - flt(rv.advance_amount) - flt(rv.loan_amount) - flt(rv.deduction_amount);
+
+    return { total_salary, amount, total_payable, balance };
+}
+
+function setup_input_handlers() {
+    $(document).off("input.salary", ".salary-editable-input");
+
+    $(document).on("input.salary", ".salary-editable-input", function () {
+        const eid = $(this).data("employee");
+        const field = $(this).data("field");
+        const value = flt($(this).val());
+
+        if (!_salary_row_data[eid]) return;
+
+        _salary_row_data[eid][field] = value;
+
+        frappe.query_report.render_datatable();
+    });
+}
--- bfl_custom/bfl_custom/report/salary_details/salary_details.py
+++ bfl_custom/bfl_custom/report/salary_details/salary_details.py
@@ -1,126 +1,265 @@
 import frappe
-from collections import defaultdict
+from frappe import _
+from frappe.utils import flt
+
 
 def execute(filters=None):
+    filters = filters or {}
+    validate_filters(filters)
+
     columns = get_columns()
     data = get_data(filters)
     return columns, data
 
+
+def validate_filters(filters):
+    if not filters.get("month"):
+        frappe.throw(_("Please select Month"))
+
+
 def get_columns():
     return [
-        {
-            "label": "Employee",
-            "fieldname": "employee",
-            "fieldtype": "Data",
-            "width": 150
-        },
-        {
-            "label": "Type",
-            "fieldname": "type",
-            "fieldtype": "Data",
-            "width": 100
-        },
-        {
-            "label": "Date",
-            "fieldname": "date",
-            "fieldtype": "Date",
-            "width": 120
-        },
-        {
-            "label": "Advance Amount",
-            "fieldname": "advance",
-            "fieldtype": "Currency",
-            "width": 130
-        },
-        {
-            "label": "Loan Paid",
-            "fieldname": "loan_paid",
-            "fieldtype": "Currency",
-            "width": 130
-        },
-        {
-            "label": "Balance",
-            "fieldname": "balance",
-            "fieldtype": "Currency",
-            "width": 130
-        }
+        {"label": _("Employee"),        "fieldname": "employee",         "fieldtype": "Link",     "options": "Employee", "width": 140},
+        {"label": _("Employee Name"),   "fieldname": "employee_name",    "fieldtype": "Data",     "width": 160},
+        {"label": _("Type"),            "fieldname": "type",             "fieldtype": "Data",     "width": 140},
+        {"label": _("Payment Type"),    "fieldname": "payment_type",     "fieldtype": "Data",     "width": 120},
+        {"label": _("Days Present"),    "fieldname": "days_present",     "fieldtype": "Float",    "width": 120},
+        {"label": _("Per Day Salary"),  "fieldname": "per_day_salary",   "fieldtype": "Currency", "width": 130},
+        {"label": _("Total Salary"),    "fieldname": "total_salary",     "fieldtype": "Currency", "width": 130},
+        {"label": _("Other Addings"),   "fieldname": "other_addings",    "fieldtype": "Currency", "width": 130},
+        {"label": _("Other Deduction"), "fieldname": "other_deduction",  "fieldtype": "Currency", "width": 130},
+        {"label": _("Amount"),          "fieldname": "amount",           "fieldtype": "Currency", "width": 120},
+        {"label": _("Total Payable"),   "fieldname": "total_payable",    "fieldtype": "Currency", "width": 130},
+        {"label": _("Advance Date"),    "fieldname": "advance_date",     "fieldtype": "Date",     "width": 120},
+        {"label": _("Advance Amount"),  "fieldname": "advance_amount",   "fieldtype": "Currency", "width": 130},
+        {"label": _("Loan Date"),       "fieldname": "loan_date",        "fieldtype": "Date",     "width": 120},
+        {"label": _("Loan Amount"),     "fieldname": "loan_amount",      "fieldtype": "Currency", "width": 120},
+        {"label": _("Deduction"),       "fieldname": "deduction_amount", "fieldtype": "Currency", "width": 120},
+        {"label": _("Balance"),         "fieldname": "balance",          "fieldtype": "Currency", "width": 130},
     ]
 
+
 def get_data(filters):
+    je_employees = get_employees_from_je(filters)
+    if not je_employees:
+        return []
 
-    month = filters.get("month")
-    employee_filter = filters.get("employee")
+    employee_ids = list(je_employees.keys())
+    emp_details = get_employee_details(employee_ids, filters)
+    advance_map = get_je_type_map(employee_ids, "Advance", filters)
+    loan_map = get_je_type_map(employee_ids, "Loan", filters)
+    deduction_map = get_deduction_map(employee_ids, filters)
 
-    conditions = ""
-    values = []
+    data = []
+    for eid, je_info in je_employees.items():
+        emp = emp_details.get(eid)
+        if not emp:
+            continue
 
-    if month:
-        conditions += " AND jea.custom_month = %s"
-        values.append(month)
+        per_day = flt(emp.get("custom_per_day_salary"))
+        if not per_day and flt(emp.get("ctc")):
+            per_day = flt(emp.get("ctc")) / 26
 
-    if employee_filter:
-        conditions += " AND jea.custom_employee = %s"
-        values.append(employee_filter)
+        adv = advance_map.get(eid, {})
+        loan = loan_map.get(eid, {})
+        ded = flt(deduction_map.get(eid, 0))
 
-    data = frappe.db.sql(f"""
-        SELECT 
-            jea.custom_employee as employee,
-            jea.custom_type,
-            je.posting_date,
-            jea.debit_in_account_currency,
-            jea.credit_in_account_currency
-        FROM `tabJournal Entry Account` jea
-        JOIN `tabJournal Entry` je ON je.name = jea.parent
-        WHERE 
-           
-            jea.custom_type IN ('Advance', 'Loan')
-            AND (
-                jea.debit_in_account_currency != 0
-                OR jea.credit_in_account_currency != 0
-            )
-            {conditions}
-        ORDER BY je.posting_date
-    """, values, as_dict=True)
-
-    grouped = defaultdict(list)
-
-    for d in data:
-        grouped[d.employee].append(d)
-
-    final = []
-
-    for emp, rows in grouped.items():
-
-        total_advance = 0
-        total_loan = 0
-
-        for r in rows:
-
-            row = {
-                "employee": emp,
-                "date": r.posting_date
-            }
-
-            amt = r.debit_in_account_currency or r.credit_in_account_currency
-
-            if r.custom_type == "Advance":
-                row["type"] = "Advance"
-                row["advance"] = amt
-                total_advance += amt
-
-            elif r.custom_type == "Loan":
-                row["type"] = "Loan"
-                row["loan_paid"] = amt
-                total_loan += amt
-
-            final.append(row)
-
-        # TOTAL ROW
-        final.append({
-            "employee": "TOTAL",
-            "advance": total_advance,
-            "loan_paid": total_loan,
-            "balance": total_loan
+        data.append({
+            "employee": eid,
+            "employee_name": emp.get("employee_name") or je_info.get("employee_name") or eid,
+            "type": je_info.get("type", ""),
+            "payment_type": emp.get("custom_payment_type", ""),
+            "days_present": 0,
+            "per_day_salary": per_day,
+            "total_salary": 0,
+            "other_addings": 0,
+            "other_deduction": 0,
+            "amount": 0,
+            "total_payable": 0,
+            "advance_date": adv.get("posting_date"),
+            "advance_amount": flt(adv.get("amount", 0)),
+            "loan_date": loan.get("posting_date"),
+            "loan_amount": flt(loan.get("amount", 0)),
+            "deduction_amount": ded,
+            "balance": 0,
         })
 
-    return final
+    return data
+
+
+def get_employees_from_je(filters):
+    conditions = [
+        "jea.party_type = 'Employee'",
+        "jea.party IS NOT NULL",
+        "jea.party != ''",
+        "jea.custom_month = %(month)s",
+    ]
+    values = {"month": filters.get("month")}
+
+    if filters.get("type"):
+        conditions.append("jea.custom_type = %(type)s")
+        values["type"] = filters.get("type")
+
+    where = " AND ".join(conditions)
+
+    rows = frappe.db.sql(
+        f"""
+        SELECT
+            jea.party AS employee,
+            e.employee_name AS employee_name,
+            jea.custom_employee AS custom_employee,
+            jea.custom_type AS type
+        FROM `tabJournal Entry Account` jea
+        LEFT JOIN `tabEmployee` e ON e.name = jea.party
+        WHERE {where}
+        
+        """,
+        values,
+        as_dict=True,
+    )
+
+    seen = {}
+    for row in rows:
+        eid = row.get("employee")
+        if eid and eid not in seen:
+            seen[eid] = {
+                "employee_name": row.get("employee_name") or row.get("custom_employee")
+                "type": row.get("type") or ""
+            }
+
+    return seen    
+    
+    conditions = [
+        "jea.custom_employee IS NOT NULL",
+        "jea.custom_employee != ''",
+        "jea.custom_month = %(month)s",
+    ]
+    values = {"month": filters.get("month")}
+
+    if filters.get("type"):
+        conditions.append("jea.custom_type = %(type)s")
+        values["type"] = filters.get("type")
+
+    where = " AND ".join(conditions)
+
+    rows = frappe.db.sql(
+        f"""
+        SELECT
+            jea.custom_employee AS employee,
+            e.name AS employee_name,
+            jea.custom_type AS type
+        FROM `tabJournal Entry Account` jea
+        LEFT JOIN `tabEmployee` e ON e.name = jea.party
+        WHERE {where}
+        ORDER BY jea.modified DESC
+        """,
+        values,
+        as_dict=True,
+    )
+
+    seen = {}
+    for row in rows:
+        eid = row.get("employee_name")
+        if eid and eid not in seen:
+            seen[eid] = {
+                "employee_name": row.get("employee_name"),
+                "type": row.get("type") or ""
+            }
+
+    return seen
+
+
+def get_employee_details(employee_ids, filters):
+    if not employee_ids:
+        return {}
+
+    conditions = ["name IN %(employees)s"]
+    values = {"employees": tuple(employee_ids)}
+
+    if filters.get("payment_type"):
+        conditions.append("custom_payment_type = %(payment_type)s")
+        values["payment_type"] = filters.get("payment_type")
+
+    if filters.get("employment_type"):
+        conditions.append("employment_type = %(employment_type)s")
+        values["employment_type"] = filters.get("employment_type")
+
+    where = " AND ".join(conditions)
+
+    rows = frappe.db.sql(
+        f"""
+        SELECT
+            name,
+            employee_name,
+            custom_payment_type,
+            employment_type,
+            ctc,
+            custom_per_day_salary
+        FROM `tabEmployee`
+        WHERE {where}
+        """,
+        values,
+        as_dict=True,
+    )
+
+    return {r["name"]: r for r in rows}
+
+
+def get_je_type_map(employee_ids, entry_type, filters):
+    if not employee_ids:
+        return {}
+
+    rows = frappe.db.sql(
+        """
+        SELECT
+            jea.custom_employee AS employee,
+            jea.posting_date AS posting_date,
+            SUM(jea.debit_in_account_currency) AS amount
+        FROM `tabJournal Entry Account` jea
+        WHERE
+            jea.custom_employee IN %(employees)s
+            AND jea.custom_month = %(month)s
+            AND jea.custom_type = %(entry_type)s
+        GROUP BY jea.custom_employee
+        """,
+        {
+            "employees": tuple(employee_ids),
+            "month": filters.get("month"),
+            "entry_type": entry_type,
+        },
+        as_dict=True,
+    )
+
+    return {
+        r["employee"]: {
+            "posting_date": r.get("posting_date"),
+            "amount": flt(r.get("amount"))
+        }
+        for r in rows
+    }
+
+
+def get_deduction_map(employee_ids, filters):
+    if not employee_ids:
+        return {}
+
+    rows = frappe.db.sql(
+        """
+        SELECT
+            jea.custom_employee AS employee,
+            SUM(jea.debit_in_account_currency) AS deduction_amount
+        FROM `tabJournal Entry Account` jea
+        WHERE
+            jea.custom_employee IN %(employees)s
+            AND jea.custom_month = %(month)s
+            AND jea.custom_is_deducted = 1
+        GROUP BY jea.custom_employee
+        """,
+        {
+            "employees": tuple(employee_ids),
+            "month": filters.get("month"),
+        },
+        as_dict=True,
+    )
+
+    return {r["employee"]: flt(r["deduction_amount"]) for r in rows}
