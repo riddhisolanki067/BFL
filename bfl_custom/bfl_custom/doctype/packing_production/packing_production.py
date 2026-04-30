2918c8bcdf6ab3b205fc01a9d29f07d3ebf7876d
Riddhi Solanki <riddhisolanki067@gmail.com>
2026-04-30 12:54:27 +0530

    opening box

--- bfl_custom/bfl_custom/doctype/packing_production/packing_production.py
+++ bfl_custom/bfl_custom/doctype/packing_production/packing_production.py
@@ -1,9 +1,33 @@
 # Copyright (c) 2026, r and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class PackingProduction(Document):
-	pass
+	def before_save(doc):
+		for row in doc.items:
+			if not (row.item and row.machine and row.contractor and doc.date):
+				row.opening_box = 0
+				continue
+
+			last_entry = frappe.db.sql("""
+				SELECT child.box_balance
+				FROM `tabPacking Production Item` child
+				INNER JOIN `tabPacking Production` parent
+					ON parent.name = child.parent
+				WHERE 
+					parent.date < %s
+					AND child.item = %s
+					AND child.machine = %s
+					AND child.contractor = %s
+				
+				ORDER BY parent.date DESC
+				LIMIT 1
+			""", (doc.date, row.item, row.machine, row.contractor), as_dict=1)
+
+			if last_entry:
+				row.opening_box = last_entry[0].box_balance
+			else:
+				row.opening_box = 0
